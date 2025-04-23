eca1cdcb15cb5f534729e3c6a3ebe6e6286a408b
Debabrata Saha <debabrata.saha@canamgroupinc.com>
2025-04-23 13:42:18 +0530

    Changed from snowflake_role to  snowflake_account_role

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "snowflake_account_role" "role"{
 resource "snowflake_grant_privileges_to_account_role" "database_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
+  account_role_name = snowflake_account_role.role.name
   on_account_object {
     object_type = "DATABASE"
     object_name = snowflake_database.db.name
@@ -38,7 +38,7 @@ resource "snowflake_schema" "schema" {
 resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
+  account_role_name = snowflake_account_role.role.name
   on_schema {
     schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
   }
@@ -47,7 +47,7 @@ resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
 resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
   provider          = snowflake.security_admin
   privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
+  account_role_name = snowflake_account_role.role.name
   on_account_object {
     object_type = "WAREHOUSE"
     object_name = snowflake_warehouse.warehouse.name
@@ -63,7 +63,7 @@ resource "snowflake_user" "user" {
     provider          = snowflake.security_admin
     name              = "tf_demo_user"
     default_warehouse = snowflake_warehouse.warehouse.name
-    default_role      = snowflake_role.role.name
+    default_role      = snowflake_account_role.role.name
     default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
     rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
@@ -71,7 +71,7 @@ resource "snowflake_user" "user" {
 resource "snowflake_grant_privileges_to_account_role" "user_grant" {
   provider          = snowflake.security_admin
   privileges        = ["MONITOR"]
-  account_role_name = snowflake_role.role.name  
+  account_role_name = snowflake_account_role.role.name  
   on_account_object {
     object_type = "USER"
     object_name = snowflake_user.user.name
@@ -80,7 +80,7 @@ resource "snowflake_grant_privileges_to_account_role" "user_grant" {
 
 resource "snowflake_grant_account_role" "grants" {
   provider  = snowflake.security_admin
-  role_name = snowflake_role.role.name
+  role_name = snowflake_account_role.role.name
   user_name = snowflake_user.user.name
 }
 
